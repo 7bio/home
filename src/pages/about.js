d0895f822a66e0da3085af2d4ce6a925192008cd
jnthnch <jchu127@gmail.com>
2019-09-01 17:33:17 -0700

    Set up project

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+
+export default () => (
+  <Layout style={{ color: `teal` }}>
+    <h1>ABOUT</h1>
+    <p>Such wow. Very React.</p>
+  </Layout >
+)
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>HOME PAGE</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+)
