a22813249e4735b511070a939934b0ce313427f1
jnthnch <jchu127@gmail.com>
2019-09-02 01:37:28 -0700

    Style header, remove default styling from ul li, add BEM block classnames, simplify jsx

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,28 +5,24 @@ import "./../styles/global.css"
 import PrimaryLogo from '../../static/images/logo_no_background.png';
 
 const ListLink = props => (
-  <li>
+  <li className="sevenbio__navbar-li">
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
 export default ({ children }) => (
   <div >
-    <header>
-      <div className="container">
-        <div className="primary-logo">
-          <img src={PrimaryLogo}></img>
-        </div>
-        <div className="nav-main">
-          <ul className="nav-links">
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/about/">About Us</ListLink>
-            <ListLink to="/team/">Our Team</ListLink>
-            <ListLink to="/news/">News</ListLink>
-            <ListLink to="/contact/">Contact</ListLink>
-          </ul>
-        </div>
+    <header className="sevenbio__header">
+      <div className="sevenbio__logo--primary">
+        <img src={PrimaryLogo}></img>
       </div>
+      <ul className="sevenbio__navbar-ul">
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About Us</ListLink>
+        <ListLink to="/team/">Our Team</ListLink>
+        <ListLink to="/news/">News</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
     </header>
     {children}
   </div>
