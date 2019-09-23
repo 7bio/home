d5eae938a98956540f96532e83d272b062e98aba
jnthnch <jchu127@gmail.com>
2019-09-22 22:26:21 -0700

    Add homepage text and background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
+import HomeBackgroundImage from '../../static/images/GPCR2-1024x606.jpg'
 
 export default () => (
   <Layout>
-    <div className="page-layout">
-      <div className="page-layout__content">
-        <p className="page-layout__text">
-          Seven biosciences is a drug discovery platform company which leverages
-          transformative technology to identify and develop novel therapeutics
-          targeting G-protein coupled receptors (GPCRs)
-        </p>
-      </div>
-      <div className="page-layout__image">
-
-      </div>
+    <div className="homepage-layout__content">
+      <img src={HomeBackgroundImage} id="homepage-layout__background-image"></img>
+      <p className="homepage-layout__text">
+        Seven biosciences is a drug discovery platform company which leverages
+        transformative technology to identify and develop<span style={{ color: '#16AA4B' }}> novel therapeutics </span>
+        targeting G-protein coupled receptors (GPCRs)
+      </p>
     </div>
-  </Layout>
+  </Layout >
 )
