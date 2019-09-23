d8eaa3ff5f991d25db45735c07bf7ba84787f8a0
jnthnch <jchu127@gmail.com>
2019-09-23 13:13:18 -0700

    Add home endpoint to links

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,14 +2,14 @@ import React from "react";
 import NavLink from './navLink';
 
 class NavBar extends React.PureComponent {
-  render () {
+  render() {
     return (
       <div className="header__links">
-        <NavLink destination='/' text='Home'/>
-        <NavLink destination='/about/' text='About'/>
-        <NavLink destination='/team/' text='Team'/>
-        <NavLink destination='/news/' text='News'/>
-        <NavLink destination='/contact' text='Contact'/>
+        <NavLink destination='/' text='Home' />
+        <NavLink destination='/home/about/' text='About' />
+        <NavLink destination='/home/team/' text='Team' />
+        <NavLink destination='/home/news/' text='News' />
+        <NavLink destination='/home/contact' text='Contact' />
       </div>);
   }
 }
