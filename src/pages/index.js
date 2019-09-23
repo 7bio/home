723103e91cc10bcc21f5ffa72e27a951757c53b3
jnthnch <jchu127@gmail.com>
2019-09-23 01:09:38 -0700

    Adjust homepage design re: feedback

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import HomeBackgroundImage from '../../static/images/GPCR2-1024x606.jpg'
 export default () => (
   <Layout>
     <div className="homepage-layout__content">
-      <img src={ HomeBackgroundImage } id="homepage-layout__background-image" alt="homepage background" />
+      <img src={HomeBackgroundImage} id="homepage-layout__background-image" alt="homepage background" />
       <p className="homepage-layout__text">
         Seven biosciences is a drug discovery platform company which leverages
-        transformative technology to identify and develop<span style={ { color: '#16AA4B' } }> novel therapeutics </span>
+        transformative technology to identify and develop novel therapeutics
         targeting G-protein coupled receptors (GPCRs)
       </p>
     </div>
