f593e799b982ad818bf02138095ecf93b2e518ed
jnthnch <jchu127@gmail.com>
2019-09-23 15:29:00 -0700

    Convert Link to a href tag

--- src/components/navLink.js
+++ src/components/navLink.js
@@ -6,7 +6,7 @@ class NavLink extends React.PureComponent {
     const { destination, text } = this.props;
     return (
       <div className="header__links__single">
-        <a to={destination} className="header__links__text" activeClassName="header__links__text--active">{text}</a>
+        <a href={destination} className="header__links__text" activeClassName="header__links__text--active">{text}</a>
         <div id="circle" />
       </div>
     );
