2f685cf87c00d747875756aa8ba1952536edf563
jnthnch <jchu127@gmail.com>
2019-09-23 13:18:01 -0700

    remove home endpoint from links

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,10 +6,10 @@ class NavBar extends React.PureComponent {
     return (
       <div className="header__links">
         <NavLink destination='/' text='Home' />
-        <NavLink destination='/home/about/' text='About' />
-        <NavLink destination='/home/team/' text='Team' />
-        <NavLink destination='/home/news/' text='News' />
-        <NavLink destination='/home/contact' text='Contact' />
+        <NavLink destination='/about/' text='About' />
+        <NavLink destination='/team/' text='Team' />
+        <NavLink destination='/news/' text='News' />
+        <NavLink destination='/contact' text='Contact' />
       </div>);
   }
 }
