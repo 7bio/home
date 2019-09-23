98c5844f4025d222da1baaf50f6f694c7704693f
jnthnch <jchu127@gmail.com>
2019-09-23 01:23:50 -0700

    Try to fix broken image

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,23 @@
 import React from "react"
 import "./layout.css"
 import "./../styles/global.css"
-import PrimaryLogo from '../../static/images/logo_no_background.png';
+import PrimaryLogo from '../static/images/logo_no_background.png';
 import NavBar from './nav.js';
 
 export default ({ children }) => (
   <div className="default">
     <div className="default__header">
       <div className="header__logo__container">
-        <img src={ PrimaryLogo } className="header__img" alt="logo for Seven Biosciences"/>
+        <img src={PrimaryLogo} className="header__img" alt="logo for Seven Biosciences" />
       </div>
       <NavBar />
     </div>
     <div className="default__body">
-      { children }
+      {children}
     </div>
     <div className="footer">
       <div className="footer__content">
-      <a href="/contact">Reach out</a> to us for more information about our services &nbsp; &nbsp; &nbsp; © 2019 Seven Biosciences, Inc.
+        <a href="/contact">Reach out</a> to us for more information about our services &nbsp; &nbsp; &nbsp; © 2019 Seven Biosciences, Inc.
       </div>
     </div>
   </div>
