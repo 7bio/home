444e0ae77d8aba70fd5127e6b87fa1d7b86b2158
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-09-23 22:46:22 -0700

    Update Team page bio and Contact page re: feedback

--- src/pages/team.js
+++ src/pages/team.js
@@ -7,38 +7,24 @@ import RuqiantPortrait from '../../static/images/ruqiang.jpg';
 
 const personData = {
   "mizuno": {
-    "photo": GracePortrait,
-    "bio": `Grace O. Mizuno, PhD
-
-    CEO, co-founder
-    
-    11 years of experience in sensor design, animal disease modeling, in vitro + in vivo testing and molecular biology (UC Davis,
-    UC San Francisco, UC San Diego, The Scripps Research Institute)
-    
-    ARCS Scholar, Keller Pathway Fellow, Loren D. Carlson dissertation prize, Max Kleiber gradate research award, 3-Minute
-    Pitch winner
-    
-    Expert in using human-iPSCs for functional disease modeling`
+    photo: GracePortrait,
+    name: 'Grace O. Mizuno, Ph.D.',
+    subheading: 'Co-Founder & CEO',
+    bio: '11 years of experience in sensor design, in vitro and in vivo functional disease modeling, biochemistry and molecular biology. ARCS Scholar, Loren D. Carlson dissertation award recipient, Max Kleiber gradate research award recipient, 3-Minute Pitch winner.',
   },
   "tian": {
-    "photo": LinPortrait,
-    "bio": `Lin Tian, PhD
-    Co-founder
-  
-    Principal Investigator at UC Davis (2012 – present)
-    20 years of experience in sensor design, protein engineering, neuroscience, biochemistry and molecular biology (UC Davis, HHMI Janelia Farm, Northwestern Univ., Univ. of Science and Technology of China)
-    NIH Director’s Innovator Award, Rita Allen Young Scholar, Hartwell Foundation Individual Biomedical Research Award, Human Frontier Science Program Young Investigator Award, HHMI Research Fellowship
-    Engineered the most utilized Ca2+ biosensor (GCaMP3) which established the basis of Obama’s BRAIN Initiative
-  Inventor on 3 patents`
+    photo: LinPortrait,
+    name: 'Lin Tian, Ph.D.',
+    subheading: 'Co-Founder',
+    bio: 'Associate Professor at UC Davis (2012 – present). Engineered the most utilized Ca2+ biosensor (GCaMP3) which established the basis of Obama’s BRAIN Initiative with 20 years of experience in sensor design, protein engineering, neuroscience, biochemistry and molecular biology. Recipient of NIH Director’s Innovator Award, Rita Allen Young Scholar, Hartwell Foundation Individual Biomedical Research Award, Human Frontier Science Program Young Investigator Award, HHMI Research Fellowship.',
   },
   "liang": {
-    "photo": RuqiantPortrait,
-    "bio": `Ruqiang Liang, Ph.D.
-    Chief Technology Officer
-    
-    24 years experience in computational biology, software engineering, molecular biology, assay development and sensor design (UC Davis, Washington University, University of Louisville, University of Kentucky, Shanghai Institutes for Biological Sciences, University of Chinese Academy of Sciences, Sichuan University)
-    Chinese Academy of Sciences first rank scholarship, Sichuan University scholarship
-  Expert in computational (in silico) guided design and modeling (Rosetta, Chimera)`
+    photo: RuqiantPortrait,
+    name: 'Ruqiang Liang, Ph.D.',
+    subheading: 'Chief Technology Officer',
+    bio: `24 years experience in computational biology, software engineering, molecular biology, assay development and sensor design (UC Davis, Washington University, University of Louisville, University of Kentucky, Shanghai Institutes for Biological Sciences, University of Chinese Academy of Sciences, Sichuan University)
+      Chinese Academy of Sciences first rank scholarship, Sichuan University scholarship
+      Expert in computational (in silico) guided design and modeling (Rosetta, Chimera)`,
   },
 };
 
@@ -46,9 +32,9 @@ export default () => (
   <Layout >
     <div className="page-layout-team page-layout">
       <h1 name='leadership' className="page-layout-team__header">Leadership</h1>
-      <OnePersonLeadership person="Grace Mizuno" bio={ personData.mizuno.bio } photo={ personData.mizuno.photo }/>
-      <OnePersonLeadership person="Lin Tian" bio={ personData.tian.bio } photo={ personData.tian.photo }/>
-      <OnePersonLeadership person="Ruqian Liang" bio={ personData.liang.bio } photo={ personData.liang.photo }/>
+      <OnePersonLeadership name={ personData.mizuno.name } subheading={ personData.mizuno.subheading } bio={ personData.mizuno.bio } photo={ personData.mizuno.photo }/>
+      <OnePersonLeadership name={ personData.tian.name } subheading={ personData.tian.subheading } bio={ personData.tian.bio } photo={ personData.tian.photo }/>
+      {/* <OnePersonLeadership person="Ruqian Liang" bio={ personData.liang.bio } photo={ personData.liang.photo }/> */}
 
       <h1 name="advisors" className="page-layout-team__header">Advisors</h1>
       <b>Lin Tian, PhD, Associate Professor</b><br/>
