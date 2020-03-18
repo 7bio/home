9eb574ee93ab1db364df38da77f45287978cf670
jnthnch <jchu127@gmail.com>
2020-03-18 15:07:52 -0700

    Install gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
   ],
 };
