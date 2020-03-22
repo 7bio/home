bd419a1f716fb40164c698388e90c072e8f9a742
jnthnch <jchu127@gmail.com>
2020-03-21 19:45:14 -0700

    Start responsive design

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,13 +4,20 @@ import NavigationButton from './NavigationButton';
 class NavigationBar extends React.PureComponent {
   render() {
     return (
-      <div className="header__links">
-        <NavigationButton destination="/" text="Home" />
-        <NavigationButton destination="/about/" text="About" />
-        <NavigationButton destination="/team/" text="Team" />
-        <NavigationButton destination="/news/" text="News" />
-        <NavigationButton destination="/contact" text="Contact" />
-      </div>
+      <>
+        <div className="nav-links">
+          <NavigationButton destination="/" text="Home" />
+          <NavigationButton destination="/about/" text="About" />
+          <NavigationButton destination="/team/" text="Team" />
+          <NavigationButton destination="/news/" text="News" />
+          <NavigationButton destination="/contact" text="Contact" />
+        </div>
+        <div className="burger">
+          <div className="line1" />
+          <div className="line2" />
+          <div className="line3" />
+        </div>
+      </>
     );
   }
 }
